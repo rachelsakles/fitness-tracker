521456307cc88f01a8895f2db9d64b2d89d2e686
Rachel Sakles <rachel.sakles@yahoo.com>
2020-09-27 22:09:10 -0700

    deploying

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 // require npm packages & models
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose"); 
-const db = require("./models");  
+const mongoose = require("mongoose");
+const db = require("./models");
 
-const PORT = process.env.PORT || 8080; 
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public")); 
+app.use(express.static("public"));
 app.use(logger("dev"));
 
 // Connect to mongodb
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
-    useNewUrlParser: true, 
-    useFindAndModify: false, 
-    useUnifiedTopology: true,  
-}); 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 // app.use(require("./routes")); 
 // Requiring our routes
@@ -31,4 +32,3 @@ require("./routes/html-routes.js")(app);
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
-  
